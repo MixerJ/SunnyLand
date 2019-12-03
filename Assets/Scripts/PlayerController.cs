b404b9031de57aaf894fe04a3545e32d92753639
zhusw <zhusw@codoon.com>
2019-12-03 17:57:58 +0800

    feature(master):add player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float PlayerSpeed;
+    public rb Ri
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+    }
+}
