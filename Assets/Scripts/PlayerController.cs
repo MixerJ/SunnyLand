2bc4b5455078355461345a35ab81192a41a6e377
zhusw <zhusw@codoon.com>
2019-12-03 20:17:28 +0800

    feature(master):complete player move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float PlayerSpeed;
-    public rb Ri
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public Rigidbody2D rb2;
+    private float xMove;
 
     // Update is called once per frame
     void Update()
@@ -18,8 +14,13 @@ public class PlayerController : MonoBehaviour
         Movement();
     }
 
+    // Player Movement
     void Movement()
     {
-
+        xMove = Input.GetAxis("Horizontal");
+        if (xMove != 0)
+        {
+            rb2.velocity = new Vector2(xMove * PlayerSpeed, rb2.velocity.y);
+        }
     }
 }
