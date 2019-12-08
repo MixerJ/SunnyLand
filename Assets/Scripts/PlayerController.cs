f8d6a7dc08e74586817eb3151d94e3bf5ff4655a
MixerJ <z846204062@hotmail.com>
2019-12-09 00:36:31 +0800

    完成跳跃

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // 玩家移动速度
     public float PlayerSpeed;
+    // 玩家跳跃高度
+    public float PlayerJumpHigher;
     public Rigidbody2D rb2;
+    // 移动
     private float xMove;
+    // 反转人物面对显示方向
+    private float faceMove;
 
     // Update is called once per frame
-    void Update()
-    {
-        Movement();
-    }
+    void FixedUpdate() => Movement();
 
     // Player Movement
     void Movement()
     {
-        xMove = Input.GetAxis("Horizontal");
+        xMove = Input.GetAxis("Horizontal");;
+        faceMove = Input.GetAxisRaw("Horizontal");
         if (xMove != 0)
         {
-            rb2.velocity = new Vector2(xMove * PlayerSpeed, rb2.velocity.y);
+            rb2.velocity = new Vector2(xMove * PlayerSpeed * Time.deltaTime, rb2.velocity.y);
+        }
+
+        if(faceMove != 0)
+        {
+            transform.localScale = new Vector3(faceMove, 1, 1);
+        }
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2.velocity = new Vector2(rb2.velocity.x, PlayerJumpHigher * Time.deltaTime);
         }
     }
 }
