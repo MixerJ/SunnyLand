5442dcc9555b62d2f20764e65be94960a654f563
zhusw <zhusw@codoon.com>
2019-12-11 13:25:12 +0800

    feature(master):增加player run和idle动画

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb2;
+    // 处理动画
+    public Animator anim;
     // 玩家移动速度
     public float PlayerSpeed;
     // 玩家跳跃高度
     public float PlayerJumpHigher;
-    public Rigidbody2D rb2;
     // 移动
     private float xMove;
     // 反转人物面对显示方向
@@ -20,18 +22,19 @@ public class PlayerController : MonoBehaviour
     // Player Movement
     void Movement()
     {
-        xMove = Input.GetAxis("Horizontal");;
+        xMove = Input.GetAxis("Horizontal"); ;
         faceMove = Input.GetAxisRaw("Horizontal");
         if (xMove != 0)
         {
             rb2.velocity = new Vector2(xMove * PlayerSpeed * Time.deltaTime, rb2.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(faceMove));
         }
 
-        if(faceMove != 0)
+        if (faceMove != 0)
         {
             transform.localScale = new Vector3(faceMove, 1, 1);
         }
-        
+
         if (Input.GetButtonDown("Jump"))
         {
             rb2.velocity = new Vector2(rb2.velocity.x, PlayerJumpHigher * Time.deltaTime);
