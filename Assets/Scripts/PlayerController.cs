45d558050b53a3bb5e6926e3e1cfc536e53d0293
zhusw <zhusw@codoon.com>
2019-12-12 13:22:31 +0800

    feature(master):完成跳跃，下降，站立动画切换

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb2;
     // 处理动画
     public Animator anim;
+    // player碰撞体
+    public Collider2D cl2;
     // 玩家移动速度
     public float PlayerSpeed;
     // 玩家跳跃高度
@@ -15,9 +17,16 @@ public class PlayerController : MonoBehaviour
     private float xMove;
     // 反转人物面对显示方向
     private float faceMove;
+    // 获取图层
+    public LayerMask ground;
+
 
     // Update is called once per frame
-    void FixedUpdate() => Movement();
+    void FixedUpdate()
+    {
+        Movement();
+        SwitchAnim();
+    }
 
     // Player Movement
     void Movement()
@@ -27,6 +36,7 @@ public class PlayerController : MonoBehaviour
         if (xMove != 0)
         {
             rb2.velocity = new Vector2(xMove * PlayerSpeed * Time.deltaTime, rb2.velocity.y);
+            //anim.SetBool(, false);
             anim.SetFloat("running", Mathf.Abs(faceMove));
         }
 
@@ -38,6 +48,25 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             rb2.velocity = new Vector2(rb2.velocity.x, PlayerJumpHigher * Time.deltaTime);
+            anim.SetBool("jumping", true);
+        }
+    }
+
+    void SwitchAnim()
+    {
+        anim.SetBool("idleing", false);
+        if (anim.GetBool("jumping"))
+        {
+            if (rb2.velocity.y < 0)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        else if (cl2.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("idleing", true);
         }
     }
 }
